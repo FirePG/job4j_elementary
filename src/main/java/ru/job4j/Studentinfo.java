961e665191902e6b6b4bb44c7c3ee4a0fe01f2fa
FirePG <paul1992.92@inbox.ru>
2023-11-26 15:19:48 +0700

    6. Checkstyle [#4123 #410613]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,9 +1,9 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main (String[] args) {
-        System.out.println ("Pavel Gritsyn");
-        System.out.println ("30.07.1992");
+    public static void main(String[] args) {
+        System.out.println("Pavel Gritsyn");
+        System.out.println("30.07.1992");
 
     }
 }
