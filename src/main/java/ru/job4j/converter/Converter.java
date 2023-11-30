0d1301ee197dd1d9c0d7fdb8b2356198182cdb98
FirePG <paul1992.92@inbox.ru>
2023-11-30 20:18:59 +0700

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,8 +3,8 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float result = value / 70;
-        return result;
+        float rsl = value / 70;
+        return rsl;
     }
 
     public static float rubleToDollar(float value) {
