6732d085e9f771d66407ba4979910fcbd9d1c9cd
FirePG <paul1992.92@inbox.ru>
2023-12-11 21:45:29 +0700

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -8,17 +8,17 @@ public class Converter {
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 20;
+        float rsl = value / 60;
         return rsl;
     }
 
     public static float euroToDollar(float value) {
-        float rsl = value * 2;
+        float rsl = value * 1.09f;
         return rsl;
     }
 
     public static float dollarToEuro(float value) {
-        float rsl = value * 0.5f;
+        float rsl = value * 0.91f;
         return rsl;
     }
 
@@ -28,18 +28,19 @@ public class Converter {
         float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result : " + passed);
-        expected = 7;
+        in = 120;
         out = Converter.rubleToDollar(in);
         passed = expected == out;
-        System.out.println("140 rubles are 7 dollars. Test result : " + passed);
-        expected = 280;
+        System.out.println("120 rubles are 7 dollars. Test result : " + passed);
+        in = 100;
+        expected = 109;
         out = Converter.euroToDollar(in);
         passed = expected == out;
-        System.out.println("140 euro are 280 dollars. Test result : " + passed);
-        expected = 70;
+        System.out.println("100 euro are 109 dollars. Test result : " + passed);
+        expected = 91;
         out = Converter.dollarToEuro(in);
         passed = expected == out;
-        System.out.println("140 dollars are 70 euro. Test result : " + passed);
+        System.out.println("100 dollars are 91 euro. Test result : " + passed);
         float euro = Converter.rubleToEuro(140);
         float dollar = Converter.rubleToDollar(140);
         float dollars = Converter.euroToDollar(140);
