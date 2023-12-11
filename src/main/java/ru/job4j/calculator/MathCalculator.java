45d0fdb22b5a1dd855b21126f473df32da2b848c
FirePG <paul1992.92@inbox.ru>
2023-12-11 23:25:30 +0700

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double minusAndDivide(double first, double second) {
+        return minus(first, second)
+                + divide(first, second);
+    }
+
+    public static double allOperations(double first, double second) {
+        return minus(first, second) + sum(first, second) + divide(first, second) + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(20, 10));
+        System.out.println("Результат расчета равен: " + minusAndDivide(20, 10));
+        System.out.println("Результат расчета равен: " + allOperations(20, 10));
+    }
+}
