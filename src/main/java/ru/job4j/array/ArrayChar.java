aae1c6b77b0f874d5b2942d5f670ad719a678366
FirePG <paul1992.92@inbox.ru>
2024-02-05 22:14:41 +0700

    6.4.0. Слово начинается с ... [#41585 #479160]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,7 +4,7 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
-        for (int i = 0; i <= prefix.length - 1; i++) {
+        for (int i = 0; i < prefix.length; i++) {
             if (prefix[i] != word[i]) {
                 result = false;
                 break;
